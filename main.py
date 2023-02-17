9d75fc4515f74d929082c41a84c16585f438f1cc
Johanne Christine Arboe Franck <jcafranck@gmail.com>
2023-02-17 12:20:19 +0100

    plots in main, save

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import scipy.io as sio
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from sklearn import metrics
-from pymatreader import read_mat
-import os
 import data
 import models
 
@@ -14,10 +7,14 @@ import models
 path_x01666 = data.path_x01666
 X,y,X_sliced = data.get_one_data(path_x01666)
 
-logregscore, x_train, x_test, y_train, y_test, predictions = models.logregr(X_sliced,y)
-models.confmat(y_test, predictions)
+plt(X_sliced)
+plt.show()
 
-MLPscore, x_train, x_test, y_train, y_test, predictions, predictions_prob = models.MLP(X_sliced,y)
-models.confmat(y_test, predictions)
+
+#logregscore, x_train, x_test, y_train, y_test, predictions = models.logregr(X_sliced,y)
+#models.confmat(y_test, predictions)
+
+#MLPscore, x_train, x_test, y_train, y_test, predictions, predictions_prob = models.MLP(X_sliced,y)
+#models.confmat(y_test, predictions)
 
 
